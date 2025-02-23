a283cf4d564745d5364ba1ede09056ca18f2d159
elknu <elknumber1@yandex.com>
2025-02-23 20:33:08 +0300

    Docker containerization added

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ source "https://rubygems.org"
 gem "rails", "~> 7.2.2", ">= 7.2.2.1"
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
 gem "sprockets-rails"
-# Use sqlite3 as the database for Active Record
+# Importing the postgres
 gem "pg"
 # Use the Puma web server [https://github.com/puma/puma]
 gem "puma", ">= 5.0"
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,36 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+#check if schools do already exist and create if not
+unless School.exists?
+  #1st school
+  School.create
+
+  #2nd school
+  School.create
+end
+
+#same with classes
+unless Schoolclass.exists?
+  #1st class
+  Schoolclass.create(letter: 'А', number: 7, students_count: 19, school_id: 1)
+
+  #2nd class
+  Schoolclass.create(letter: 'Б', number: 8, students_count: 21, school_id: 1)
+
+  #3rd class
+  Schoolclass.create(letter: 'В', number: 9, students_count: 23, school_id: 2)
+end
+
+#and students
+unless Student.exists?
+  #1st student
+  Student.create(first_name: 'Павел', last_name: 'Сергеевич', surname: 'Тетюхин', school_id: 1, schoolclass_id: 1)
+
+  #2nd student
+  Student.create(first_name: 'Алина', surname: 'Пязок', school_id: 1, schoolclass_id: 1)
+
+  #3rd student
+  Student.create(first_name: 'Александр', last_name: 'Павлович', surname: 'Шилоносов', school_id: 2, schoolclass_id: 3)
+end
